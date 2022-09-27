109aaeebd9bc0281ea2aa7257cecc162f554f94e
aniyuksh <ozaaniyuksh@gmail.com>
2022-09-27 16:07:34 +0530

    Done with the main logic

--- app.js
+++ app.js
@@ -14,8 +14,43 @@ var txtValue = document.querySelector("#txt-input");
 
 // Exercise 5
 // var txtValue = document.querySelector("#txt-input");
-var divOutput = document.querySelector("#div-result");
-function clickEventHandler() {
-  divOutput.innerText = "Ssdads "+txtValue.value;
+// var divOutput = document.querySelector("#div-result");
+// function clickEventHandler() {
+//   divOutput.innerText = "Ssdads "+txtValue.value;
+// }
+// btnValue.addEventListener("click", clickEventHandler);
+
+// Exercise 6 
+// It has been done in Postman
+// By posting the URL in the search bar and then added values in the below fields, and then call the get button.
+
+// Exercise 7
+var clickButton = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#txt-input");
+var outputText = document.querySelector("#txt-output");
+
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getUrl(text){
+  return url + "?" + "text=" + text;
 }
-btnValue.addEventListener("click", clickEventHandler);
+
+function errorHandler(error){
+  console.log("OOPS, error occurred.!");
+  alert("Please try after some time...!")
+}
+function callEvent(){
+  var inpVal = inputText.value;
+
+  fetch(getUrl(inpVal))
+  .then(response => response.json())
+ 
+  .then(json => {
+  var res = json.contents.translated;
+  outputText.innerText = res})
+  .catch(errorHandler)
+};
+
+clickButton.addEventListener("click", callEvent);
+
+
